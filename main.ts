b279d8a0953a0dbe4e9c8474cb2cfb8dffce9f3e
Jens Dalsgaard <mr.dalsgaard@gmail.com>
2024-12-08 15:19:42 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,16 @@
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(teamNumber)
+})
+radio.onReceivedValue(function (name, value) {
+    if ("teamResult".compare(name) > "0" && value == "0") {
+        basic.showIcon(IconNames.Happy)
+    } else if ("teamResult".compare(name) > "0") {
+        basic.showNumber(value)
+    } else {
+    	
+    }
+})
+let teamNumber = 0
+teamNumber = 1
+radio.setGroup(1)
+basic.showNumber(teamNumber)
